992849391e4144ed0bcec0ffc6958b6a0deb5aed
kmpk <kmpk.gm@gmail.com>
2024-03-01 14:03:11 +0400

    Add test data and test config

--- twitch/twitch-microservice/src/test/java/com/github/kmpk/twitch/TestData.java
+++ twitch/twitch-microservice/src/test/java/com/github/kmpk/twitch/TestData.java
@@ -8,6 +8,12 @@ import java.time.Instant;
 
 public class TestData {
     public static final TwitchLiveStreamTo NEW_STREAM_TO = new TwitchLiveStreamTo("newId", "newLogin", "newUser", "newId", "newGame", Instant.now(), "newTitle", "url");
+
+    public static final TwitchUserTo EXISTED_USER_TO = new TwitchUserTo("existedId", "existedLogin", "existedName", "existedAvatarUrl");
+    public static final TwitchUserTo EXISTED_OLD_USER_TO = new TwitchUserTo("existedOldId", "existedOldLogin", "existedOldName", "existedOldAvatarUrl");
     public static final TwitchUserTo NEW_USER_TO = new TwitchUserTo("newId", "newLogin", "newName", "newAvatarUrl");
+
+    public static final TwitchGameTo EXISTED_GAME_TO = new TwitchGameTo("existedId", "existedName", "existedUrl");
+    public static final TwitchGameTo EXISTED_OLD_GAME_TO = new TwitchGameTo("existedOldId", "existedOldName", "existedOldUrl");
     public static final TwitchGameTo NEW_GAME_TO = new TwitchGameTo("newId", "newName", "newUrl");
 }
